54b8a8a5c70c59a3f80ac163e902169a4410da25
Prince SBI <shreyasgs403@gmail.com>
2023-01-11 18:05:54 +0530

    Initial commit

--- food-recognition-smart-brain-frontend/src/components/Footer/Footer.js
+++ food-recognition-smart-brain-frontend/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer'>
-            <a  className='f4 link dim light-gray pointer' href='https://github.com/PrinceSBI' target="_blank" rel="noopener noreferrer">GitHub-Link</a>
+            <a  className='f4 link dim light-gray pointer' href='https://github.com/PrinceSBI/Smart-Brain-Food-Recognition-Website' target="_blank" rel="noopener noreferrer">GitHub-Link</a>
         </div>
     );
 }
